8ebf0d52aed9d3bcc7fc176c1fc86de787f7748e
Trelta Sebastion <treltasev@gmail.com>
2025-01-27 18:10:01 -0500

    sync/ pc

---
+++ TablesProject/src/lib/ui/Table.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let headers = [
+        "Name",
+        "Maths",
+        "Science",
+        "English",
+        "Physics",
+        "General Knowledge"
+    ]
+    
+    export let data: (any)[][] = [
+        ["David", 85, 87, 88, 92, 88],
+        ["Richard", 85, 87, 88, 92, 88],
+        ["John", 85, 87, 88, 92, 88],
+        ["David", 85, 87, 88, 92, 88],
+        ["David", 85, 87, 88, 92, 88],
+    ]
+</script>
